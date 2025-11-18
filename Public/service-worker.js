fa51b1a331298d4dfad62ff0a2f93168edbefa26
fireman4815-cpu <fireman4815@gmail.com>
2025-11-18 15:04:25 -0500

    Delete Public directory

--- Public/service-worker.js
+++
@@ -1,9 +0,0 @@
-// Basic service worker to enable PWA installability
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (event) => {
-  // Network-first strategy
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
